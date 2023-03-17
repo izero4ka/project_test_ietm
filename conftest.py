c8a8bac47fc78302a42b511531b4810f85d5bd88
Dmitriy Samarov <izero4ka@yandex.ru>
2023-03-17 09:27:08 +0300

    комент

--- conftest.py
+++ conftest.py
@@ -21,22 +21,22 @@ def browser(request):  # определяет функцию browser, котор
     browser_name = request.config.getoption(
         "browser_name")  # получает значение параметра --browser_name из командной строки, который указывает, какой браузер должен быть запущен
     user_language = request.config.getoption(
-        "language")  # получает значение параметра --language из командной строки, который указывает, на каком языке должны быть локализованы веб-страницы.
+        "language")  # получает значение параметра --language из командной строки, который указывает, на каком языке должны быть локализованы веб-страница
     browser = None  # создает переменную browser и присваивает ей значение None
     if browser_name == "Chrome":  # проверяет, какой браузер был выбран для тестирования
         options = Options()  # создает экземпляр класса Options, используемый для настройки предпочтений пользователя в Chrome
         options.add_experimental_option('prefs', {
-            'intl.accept_languages': user_language})  # добавляет параметр 'intl.accept_languages' для опции 'prefs' класса Options, который указывает нужный пользовательский язык.
+            'intl.accept_languages': user_language})  # добавляет параметр 'intl.accept_languages' для опции 'prefs' класса Options, который указывает нужный пользовательский язык
         print(
-            "\nЗапуск браузера Chrome для теста..")  # добавляет параметр 'intl.accept_languages' для опции 'prefs' класса Options, который указывает нужный пользовательский язык
+            "\nЗапуск браузера Chrome для теста..")  # выводит сообщение о том, что браузер Chrome запускается для тестирования
         browser = webdriver.Chrome(
             options=options)  # создает экземпляр класса webdriver для браузера Firefox с настройками, определенными в профиле Firefox
-    elif browser_name == "Firefox":  # если был выбран неверный браузер, то вызывается исключение
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference("intl.accept_languages", user_language)
-        print("\nЗапуск браузера Firefox для теста..")
-        browser = webdriver.Firefox(firefox_profile=fp)
-    else:
+    elif browser_name == "Firefox":  # проверяет, был ли выбран браузер Firefox для тестирования
+        fp = webdriver.FirefoxProfile() # создает экземпляр профиля браузера Firefox
+        fp.set_preference("intl.accept_languages", user_language) # устанавливает настройку "intl.accept_languages" для профиля браузера Firefox для выбранного пользовательского языка
+        print("\nЗапуск браузера Firefox для теста..") # выводит сообщение о том, что браузер Firefox запускается для тестирования
+        browser = webdriver.Firefox(firefox_profile=fp) # создает экземпляр класса webdriver для браузера Firefox с настройками, определенными в профиле Firefox
+    else: # если был выбран неверный браузер, то вызывается исключение
         raise pytest.UsageError(
             "--browser_name должно быть Chrome или Firefox")  # вызывает ошибку использования TypeError, если параметр --browser_name был указан неверно
     yield browser  # возвращает браузер
