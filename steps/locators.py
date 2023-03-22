aeaae239fc65c291b7ab82ccfe7c09e07c2d962f
Dmitriy Samarov <izero4ka@yandex.ru>
2023-03-22 16:04:05 +0300

    добавил локаторы

--- steps/locators.py
+++ steps/locators.py
@@ -1,12 +1,22 @@
 from selenium.webdriver.common.by import By
 
 class BasePageLocators():
-    CHAPTER_SELECTION = (By.CSS_SELECTOR, "#login_link")
-    PLANNED_MAINTENANCE = (By.CSS_SELECTOR, "#login_link")
-    CURRENT_REPAIRS = (By.CSS_SELECTOR, "#login_link")
-    DOCUMENTATION_SEARCH = (By.CSS_SELECTOR, "#login_link")
-    SCENARIOS = (By.CSS_SELECTOR, "#login_link")
-    COLLECTION_OPERATIONAL_DATA = (By.CSS_SELECTOR, "#login_link")
-    LIBRARY = (By.CSS_SELECTOR, "#login_link")
-    INFORMATION_ABOUT_ERD = (By.CSS_SELECTOR, "#login_link")
-    PRODUCT_CONFIGURATION_SELECTION = (By.CSS_SELECTOR, "#login_link")
+    CHAPTER_SELECTION = (By.CSS_SELECTOR, "div.m-btn:nth-child(2)")
+    PLANNED_MAINTENANCE = (By.CSS_SELECTOR, "div.m-btn:nth-child(3)")
+    CURRENT_REPAIRS = (By.CSS_SELECTOR, "div.m-btn:nth-child(4)")
+    DOCUMENTATION_SEARCH = (By.CSS_SELECTOR, "div.m-btn:nth-child(5)")
+    SCENARIOS = (By.CSS_SELECTOR, "div.m-btn:nth-child(6)")
+    COLLECTION_OPERATIONAL_DATA = (By.CSS_SELECTOR, "div.m-btn:nth-child(7)")
+    LIBRARY = (By.CSS_SELECTOR, "a.mt-12:nth-child(3)")
+    INFORMATION_ABOUT_ERD = (By.CSS_SELECTOR, ".mr-10")
+    PRODUCT_CONFIGURATION_SELECTION = (By.CSS_SELECTOR, ".ml-10")
+
+class ChoisePublication():
+    OPERATOR_MANUAL = (By.CSS_SELECTOR, "div.row:nth-child(3) > div:nth-child(1)")
+    MAINTENANCE_MANUAL = (By.CSS_SELECTOR, "div.col-md-3:nth-child(2)")
+    OPERATING_ENGINE_MANUAL = (By.CSS_SELECTOR, "div.row:nth-child(3) > div:nth-child(3)")
+    OPERATION_URAL_MANUAL = (By.CSS_SELECTOR, "div.col-md-3:nth-child(4)")
+class MainLocators():
+    BACK_BLUE = (By.CSS_SELECTOR, ".mdi-arrow-left")
+    PROPERTIES_MD = (By.CSS_SELECTOR, ".mdi-information")
+
