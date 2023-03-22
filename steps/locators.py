06024f90b2b5bb092c2ff824e2e540e0ca50fe61
Dmitriy Samarov <izero4ka@yandex.ru>
2023-03-22 11:17:32 +0300

    добавил локаторы основной страницы

---
+++ steps/locators.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+
+class BasePageLocators():
+    CHAPTER_SELECTION = (By.CSS_SELECTOR, "#login_link")
+    PLANNED_MAINTENANCE = (By.CSS_SELECTOR, "#login_link")
+    CURRENT_REPAIRS = (By.CSS_SELECTOR, "#login_link")
+    DOCUMENTATION_SEARCH = (By.CSS_SELECTOR, "#login_link")
+    SCENARIOS = (By.CSS_SELECTOR, "#login_link")
+    COLLECTION_OPERATIONAL_DATA = (By.CSS_SELECTOR, "#login_link")
+    LIBRARY = (By.CSS_SELECTOR, "#login_link")
+    INFORMATION_ABOUT_ERD = (By.CSS_SELECTOR, "#login_link")
+    PRODUCT_CONFIGURATION_SELECTION = (By.CSS_SELECTOR, "#login_link")
